ca5b9ae1c4efcbe5308111dc7966a0164e4cbae3
Marcos Arruda <marcoswca1@gmail.com>
2019-03-13 17:46:29 -0300

    Adding new fields

--- server/resources/comment/comment.model.js
+++ server/resources/comment/comment.model.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-    text: {
+    comment: {
         type: String,
         required: true
     },
-    user: {
+    rating: {
+        type: Number,
+        required: true
+    },
+    author: {
         type: Schema.Types.ObjectId,
         required: true,
         ref: 'User'
